053dceebbd7a9a8a65c594fd3b9e937bd7dbfe1b
SeifRashedd <seifrashedswe@gmail.com>
2024-12-13 06:02:12 +0200

    first commit prepare the app

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const BabylonApp());
+}
+
+class BabylonApp extends StatelessWidget {
+  const BabylonApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
+  }
+}
